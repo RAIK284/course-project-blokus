a3af1892f42c0adcdaa8f26b748e3bab8403a696
heinrichsophia <heinrichs6739@gmail.com>
2024-03-02 21:14:39 -0600

    Took out svgs. Buttons light up

---
+++ frontend/src/components/home page pieces/GreenBlock.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./GreenBlock.css";
+
+function GreenBlock() {
+  return (
+    <div id="greenBlock">
+      <div id="greenPiece"></div>
+      <div id="greenPiece"></div>
+      <div id="greenPiece"></div>
+      <div id="clearPiece"></div>
+      <div id="greenPiece"></div>
+      <div id="clearPiece"></div>
+    </div>
+  );
+}
+
+export default GreenBlock;
---
+++ frontend/src/components/home page pieces/RedBlock.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./RedBlock.css";
+
+function RedBlock() {
+  return (
+    <div id="redBlock">
+      <div id="redPiece"></div>
+      <div id="clearPiece"></div>
+      <div id="clearPiece"></div>
+      <div id="clearPiece"></div>
+      <div id="redPiece"></div>
+      <div id="redPiece"></div>
+      <div id="redPiece"></div>
+      <div id="redPiece"></div>
+      <div id="clearPiece"></div>
+      <div id="clearPiece"></div>
+      <div id="clearPiece"></div>
+      <div id="redPiece"></div>
+    </div>
+  );
+}
+
+export default RedBlock;
