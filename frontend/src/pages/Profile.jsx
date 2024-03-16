7b294d7142cd500bad020bfe951d8a8d6fe89bab
Allan Muinov <amuinov2@huskers.unl.edu>
2024-03-15 21:40:51 -0500

    Profile info

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -2,7 +2,32 @@ import "./Profile.css";
 import { useState } from "react";
 
 function Profile() {
-  return <div id="profilePage">Profile</div>;
+  // variable for name (useState)
+  const [nickname, setNickname] = useState("Allan");
+  const [email, setEmail] = useState("allan.muinov@gmail.com");
+  const [password, setPassword] = useState("Hello");
+
+  return (
+    <div id="profile">
+      Your Profile
+      <div id="profilebox">
+        <div id="imagebox"></div>
+        <div id="infocontainer">
+          <div id="infotext">
+            <div class="infobox">Nickname:</div>
+            <div class="infobox">Email:</div>
+            <div class="infobox">Password:</div>
+          </div>
+          <div id="inputtext">
+            <div class="textbox">{nickname}</div>
+            <div class="textbox">{email}</div>
+            <div class="textbox">{password}</div>
+          </div>
+        </div>
+      </div>
+      <div id="logoutbutton">Log Out</div>
+    </div>
+  );
 }
 
 export default Profile;
