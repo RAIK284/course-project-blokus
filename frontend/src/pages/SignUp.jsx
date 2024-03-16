f93335864650e14e2d744a520aa0dbbb7a621abb
Allan Muinov <amuinov2@huskers.unl.edu>
2024-03-15 21:25:45 -0500

    Complete Sign Up Page layout.
    Sign Up Page layout completed. No edit functionality.

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,43 +1,34 @@
 import "./SignUp.css";
+import { useState } from "react";
 
 function SignUp() {
+  // variable for name (useState)
+  const [nickname, setNickname] = useState("BabeBabeBabe42");
+  const [email, setEmail] = useState("allan.muinov@gmail.com");
+  const [password, setPassword] = useState("******");
+  const [confirmpassword, setConfirmPassword] = useState("******");
 
   return (
     <div id="signup">
-      Sign Up Now!
-      <div id="profilebox">
-        <div id="imagebox">
-        </div>
-        <div id="userinfo">
-          <div id="infobox">
-            Nickname:
-            <br></br>
-            <br></br>
-            <div id="textbox">
-            <input type="text" size="40"/>
-            </div>
+      Create an Account to Play!
+      <div id="signupbox">
+        <div id="suimagebox"></div>
+        <div id="suinfocontainer">
+          <div id="suinfotext">
+            <div class="suinfobox">Nickname:</div>
+            <div class="suinfobox">Email:</div>
+            <div class="suinfobox">Password:</div>
+            <div class="suinfobox">Confirm Password:</div>
           </div>
-          <div id="infobox">
-            Username:
-            <br></br>
-            <br></br>
-            <div id="textbox">
-            <input type="text" size="40"/>
-            </div>
-          </div>
-          <div id="infobox">
-            Password:
-            <br></br>
-            <br></br>
-            <div id="textbox">
-            <input type="text" size="40"/>
-            </div>
+          <div id="suinputtext">
+            <div class="sutextbox">{nickname}</div>
+            <div class="sutextbox">{email}</div>
+            <div class="sutextbox">{password}</div>
+            <div class="sutextbox">{confirmpassword}</div>
           </div>
         </div>
       </div>
-      <div id="logoutbutton">
-        Log Out
-      </div>
+      <div id="signupbutton">Sign Up</div>
     </div>
   );
 }
