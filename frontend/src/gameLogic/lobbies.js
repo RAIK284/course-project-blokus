4420c7253278a21f519779e8e4253f0aaba24caf
Emmett Myers <emmettleemyers@gmail.com>
2024-04-15 11:06:12 -0500

    Keeping exports

--- frontend/src/gameLogic/lobbies.js
+++ frontend/src/gameLogic/lobbies.js
@@ -1,7 +1,6 @@
 import io from 'socket.io-client';
 import { board_matrix } from './board';
 
-/*
 export const socket = io('http://localhost:5000');
 export let in_online_game = false;
 export let lobby_code = -1;
@@ -16,43 +15,43 @@ export function set_in_online_game(value){
 }
 
 export const create_game = () => {
-    socket.emit('create_game', {
-        playerId: player_id
-    });
+    //socket.emit('create_game', {
+    //    playerId: player_id
+    //});
 }
 
 export const start_game = (lobbyCode) => {
-    socket.emit('start_game', {
-        lobbyCode: lobbyCode
-    });
+    //socket.emit('start_game', {
+    //    lobbyCode: lobbyCode
+    //});
 }
 
 export const join_game = (lobbyCode) => {
-    socket.emit('join_game', { 
-        lobbyCode: lobbyCode,
-        playerId: player_id
-    });
+    //socket.emit('join_game', { 
+    //    lobbyCode: lobbyCode,
+    //    playerId: player_id
+    //});
 }
 
 export const find_open_game = () => {
-    socket.emit('find_open_game', {
-        playerId: player_id
-    });
+    //socket.emit('find_open_game', {
+    //    playerId: player_id
+    //});
 }
 
 export const piece_played = (lobbyCode, board) => {
-    socket.emit('piece_played', { 
-        lobbyCode: lobbyCode,
-        board: board,
-        playerId: player_id
-    });
+    //socket.emit('piece_played', { 
+    //    lobbyCode: lobbyCode,
+    //    board: board,
+    //    playerId: player_id
+    //});
 }
 
+/*
 socket.on('connect', () => {
     console.log('Connected to server');
 });
 
 socket.on('disconnect', () => {
     console.log('Disconnected from server');
-});
-*/
+});*/
