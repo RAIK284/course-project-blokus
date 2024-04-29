ca79b68788637747b2d92b5c5ae24eed136cd8dc
LenaLankas <llankas2@huskers.unl.edu>
2024-04-29 12:22:53 -0500

    Updated

--- frontend/src/pages/Leaderboard.jsx
+++ frontend/src/pages/Leaderboard.jsx
@@ -8,19 +8,9 @@ import { useAuth } from "./Auth/AuthContext";
 
 function Leaderboard() {
 
-  const { authUser, setAuthUser } = useAuth();
+  const { authUser } = useAuth();
   const [userData, setUserData] = useState("");
-  const [usersData, setUsersData] = useState("");
-
-
-
-  const sortedData = [];
-  // const [nickname, setNickname] = useState("Loading ...");
-  // const [email, setEmail] = useState("Loading ...");
-  // const [editMode, setEditMode] = useState(false);
-  // const [isNicknameDirty, setIsNicknameDirty] = useState(false);
-  // const [isEmailDirty, setIsEmailDirty] = useState(false);
-  // const [message, setMessage] = useState("");
+  const [usersData, setUsersData] = useState([]);
 
   // get all the firestore info for the current logged in user
   useEffect(() => {
@@ -48,48 +38,53 @@ function Leaderboard() {
 
   // get all the firestore info for the users
   useEffect(() => {
-      const getUsersData = async () => {
-        try {
-          const usersCollectionRef = collection(database, "users");
-          const usersSnapshot = await getDocs(usersCollectionRef);
+    const getUsersData = async () => {
+      try {
+        const usersCollectionRef = collection(database, "users");
+        const usersSnapshot = await getDocs(usersCollectionRef);
 
-          const usersData = [];
-          usersSnapshot.forEach(doc => {
-            if (doc.exists()) {
-              usersData.push(doc.data());
-            } else {
-              console.log("Document does not exist for user with ID:", doc.id);
-            }
-          });
-
-          console.log(usersData.sort((a, b) => b.totalPieces - a.totalPieces));
-        } catch (error) {
-          console.error("Error fetching users data:", error);
-        }
-      };
-
-      getUsersData();
+        const usersData = [];
+        usersSnapshot.forEach((doc) => {
+          if (doc.exists()) {
+            usersData.push(doc.data());
+          } else {
+            console.log("Document does not exist for user with ID:", doc.id);
+          }
+        });
+        usersData.sort((a, b) => b.totalPieces - a.totalPieces);
+        console.log(usersData);
+      } catch (error) {
+        console.error("Error fetching users data:", error);
+      }
+    };
+    getUsersData();
   }, []);
 
-  // sorting all the users 
-  // sortedData = getUsersData().sort(function(a, b) {
-  //   return parseFloat(a.totalPieces) - parseFloat(b.totalPieces);
-  // });
+  const currentUserRank = () => {
+    usersData.findIndex(current => current.id === userData.id)
+  }; 
+
+  const getTopTen = () => {
+    setUsersData(usersData.slice(0, 10));
+  };
+
+const userIsInTopTen = () => {
+    getTopTen();
+    (usersData.find(current => current.id === userData.id) !== undefined);
+};
 
 
-  
+  console.log(userIsInTopTen());
 
-
-  
   return <div id="leaderboard">
 
     <div id="individualContainer">
       <div id="individualTitle">{userData.nickname}'s stats</div>
       <img id="individualAvatar" src={AvatarIcon} alt="Avatar" />
       <div className="individualInfo">rank: #1</div>
-      <div className="individualInfo">score: 97</div>
-      <div className="individualInfo">wins: 24</div>
-      <div className="individualInfo">losses: 2</div>
+      <div className="individualInfo">score: {userData.totalPieces}</div>
+      <div className="individualInfo">wins: {userData.gamesWon}</div>
+      <div className="individualInfo">losses: {userData.gamesPlayed - userData.gamesWon}</div>
     </div>
 
     <div id="globalContainer">
@@ -100,18 +95,15 @@ function Leaderboard() {
         <div>Score</div>
       </div>
       <div id="rankings">
-        <Ranking rank="1" name="catluver123" score="97" />
-        <Ranking rank="2" name="user38501346" score="85" />
-        <Ranking rank="3" name="player3" score="80" />
-        <Ranking rank="4" name="player4" score="74" />
-        <Ranking rank="5" name="chicken" score="36" />
-        <Ranking rank="6" name="PikePresident" score="35" />
-        <Ranking rank="7" name="user023487" score="29" />
-        <Ranking rank="8" name="IDK" score="23" />
-        <Ranking rank="9" name="funfriend" score="18" />
-        <Ranking rank="10" name="iloveblocks" score="17" />
-        <div id="dotdotdot">. . .</div>
-        <Ranking rank="45" name={userData.nickname} score={userData.totalPieces} />
+        {usersData.map((oneUsersData, index) => (
+          <Ranking rank={index+1} name={oneUsersData.nickname} score={oneUsersData.totalPieces} />
+        ))}
+
+        {(!userIsInTopTen()) &&
+          (<><div id="dotdotdot">. . .</div>
+            <Ranking rank={currentUserRank} name={userData.nickname} score={userData.totalPieces} /> </>)
+        }
+
       </div>
     </div>
 
