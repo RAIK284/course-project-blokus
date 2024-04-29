c3752b3ee87f2bddf23986de37dca3200c56614d
heinrichsophia <heinrichs6739@gmail.com>
2024-04-29 12:32:45 -0500

    display ranking

--- frontend/src/pages/Leaderboard.jsx
+++ frontend/src/pages/Leaderboard.jsx
@@ -7,10 +7,10 @@ import { doc, getDoc, setDoc, getDocs, collection } from "firebase/firestore";
 import { useAuth } from "./Auth/AuthContext";
 
 function Leaderboard() {
-
   const { authUser } = useAuth();
   const [userData, setUserData] = useState("");
   const [usersData, setUsersData] = useState([]);
+  const [currentUserRank, setCurrentUserRank] = useState();
 
   // get all the firestore info for the current logged in user
   useEffect(() => {
@@ -19,7 +19,6 @@ function Leaderboard() {
         const userDocRef = doc(database, "users", authUser.uid);
         const userDocSnapshot = await getDoc(userDocRef);
         if (userDocSnapshot.exists()) {
-
           const userData = userDocSnapshot.data();
           console.log(userData);
           setUserData(userData);
@@ -36,7 +35,7 @@ function Leaderboard() {
     }
   }, [authUser]);
 
-  // get all the firestore info for the users
+  // fetch array of all user data
   useEffect(() => {
     const getUsersData = async () => {
       try {
@@ -52,62 +51,70 @@ function Leaderboard() {
           }
         });
         usersData.sort((a, b) => b.totalPieces - a.totalPieces);
+        setUsersData(usersData.slice(0, 10));
+
+        // Now that usersData and userData are set, find currentUserRank
+        const rank = usersData.findIndex(
+          (current) => current.id === userData.id
+        );
+        setCurrentUserRank(rank === -1 ? null : rank + 1);
+
         console.log(usersData);
       } catch (error) {
         console.error("Error fetching users data:", error);
       }
     };
     getUsersData();
-  }, []);
+  }, [userData]); // Update when userData changes
 
-  const currentUserRank = () => {
-    usersData.findIndex(current => current.id === userData.id)
-  }; 
+  const userIsInTopTen =
+    usersData.find((current) => current.id === userData.id) !== undefined;
 
-  const getTopTen = () => {
-    setUsersData(usersData.slice(0, 10));
-  };
+  console.log(currentUserRank);
 
-const userIsInTopTen = () => {
-    getTopTen();
-    (usersData.find(current => current.id === userData.id) !== undefined);
-};
-
-
-  console.log(userIsInTopTen());
-
-  return <div id="leaderboard">
-
-    <div id="individualContainer">
-      <div id="individualTitle">{userData.nickname}'s stats</div>
-      <img id="individualAvatar" src={AvatarIcon} alt="Avatar" />
-      <div className="individualInfo">rank: #1</div>
-      <div className="individualInfo">score: {userData.totalPieces}</div>
-      <div className="individualInfo">wins: {userData.gamesWon}</div>
-      <div className="individualInfo">losses: {userData.gamesPlayed - userData.gamesWon}</div>
-    </div>
-
-    <div id="globalContainer">
-      <div id="globalTitle">Global Leaderboard</div>
-      <div id="subtitles">
-        <div>Rank</div>
-        <div id="nameMargin">Name</div>
-        <div>Score</div>
+  return (
+    <div id="leaderboard">
+      <div id="individualContainer">
+        <div id="individualTitle">{userData.nickname}'s stats</div>
+        <img id="individualAvatar" src={AvatarIcon} alt="Avatar" />
+        <div className="individualInfo">rank: #{currentUserRank + 1}</div>
+        <div className="individualInfo">score: {userData.totalPieces}</div>
+        <div className="individualInfo">wins: {userData.gamesWon}</div>
+        <div className="individualInfo">
+          losses: {userData.gamesPlayed - userData.gamesWon}
+        </div>
       </div>
-      <div id="rankings">
-        {usersData.map((oneUsersData, index) => (
-          <Ranking rank={index+1} name={oneUsersData.nickname} score={oneUsersData.totalPieces} />
-        ))}
 
-        {(!userIsInTopTen()) &&
-          (<><div id="dotdotdot">. . .</div>
-            <Ranking rank={currentUserRank} name={userData.nickname} score={userData.totalPieces} /> </>)
-        }
+      <div id="globalContainer">
+        <div id="globalTitle">Global Leaderboard</div>
+        <div id="subtitles">
+          <div>Rank</div>
+          <div id="nameMargin">Name</div>
+          <div>Score</div>
+        </div>
+        <div id="rankings">
+          {usersData.map((oneUsersData, index) => (
+            <Ranking
+              rank={index + 1}
+              name={oneUsersData.nickname}
+              score={oneUsersData.totalPieces}
+            />
+          ))}
 
+          {!userIsInTopTen && (
+            <>
+              <div id="dotdotdot">. . .</div>
+              <Ranking
+                rank={currentUserRank}
+                name={userData.nickname}
+                score={userData.totalPieces}
+              />{" "}
+            </>
+          )}
+        </div>
       </div>
     </div>
-
-  </div>;
+  );
 }
 
 export default Leaderboard;
