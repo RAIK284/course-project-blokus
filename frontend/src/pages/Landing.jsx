256f9499abc36ef0caf47687ac70b8d0a4966f20
Emmett Myers <emmettleemyers@gmail.com>
2024-05-07 12:01:23 -0500

    Landing page animation

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -4,18 +4,34 @@ import { Link } from "react-router-dom";
 
 function Landing() {
   return (
-    <div id="landing">
-      <span id="landingTitle">BLOKUS</span>
-      <div id="landingButtons">
-        <Link id="loginButton" to="/login">
-          <div>Log In</div>
-        </Link>
-        <div style={{ width: "6em" }}></div>
-        <Link id="signUpButton" to="/signup">
-          Sign Up
-        </Link>
+    <>
+      <div id="landing">
+        <span id="landingTitle">BLOKUS</span>
+        <div id="landingButtons">
+          <Link id="loginButton" to="/login">
+            <div>Log In</div>
+          </Link>
+          <div style={{ width: "6em" }}></div>
+          <Link id="signUpButton" to="/signup">
+            Sign Up
+          </Link>
+        </div>
       </div>
-    </div>
+      <div class="area" >
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div >
+    </>
   );
 }
 
