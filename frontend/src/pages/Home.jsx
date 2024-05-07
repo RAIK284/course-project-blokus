50d3621568385333f80a709bf2f19c6044e3ae70
Emmett Myers <emmettleemyers@gmail.com>
2024-05-07 11:22:27 -0500

    Home animation bug fixes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Home.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import StartOrJoinGameModal from "../components/modals/StartOrJoinGameModal.jsx";
 import "../components/PieceBlock.css";
 import RedBlock from "../components/home page pieces/RedBlock.jsx";
@@ -10,6 +10,7 @@ import BlueBlock from "../components/home page pieces/BlueBlock.jsx";
 function Home() {
   const [isStartOrJoinGameModalOpen, setStartOrJoinGameModalOpen] =
     useState(false);
+  const navigate = useNavigate();
 
   const openStartOrJoinGameModal = () => {
     setStartOrJoinGameModalOpen(true);
@@ -19,18 +20,24 @@ function Home() {
     setStartOrJoinGameModalOpen(false);
   };
 
+  const navigateLeaderboard = () => {
+    navigate(`/leaderboard`);
+  };
+
+  const navigateProfile = () => {
+    navigate(`/profile`);
+  };
+
   return (
     <>
       <div className="Home">
         <span className="home-title">BLOKUS</span>
 
         <div className="home-page-buttons">
-          <Link to="/leaderboard">
-            <div className="leaderboard-button-container">
-              <RedBlock />
-              <span className="leaderboard-text">Leaderboard</span>
-            </div>
-          </Link>
+          <div className="leaderboard-button-container" onClick={navigateLeaderboard}>
+            <RedBlock />
+            <span className="leaderboard-text">Leaderboard</span>
+          </div>
 
           <div className="horizontal-gap"></div>
 
@@ -44,12 +51,10 @@ function Home() {
 
           <div className="horizontal-gap"></div>
 
-          <Link to="/profile">
-            <div className="profile-button-container">
-              <BlueBlock />
-              <span className="profile-text">Profile</span>
-            </div>
-          </Link>
+          <div className="profile-button-container" onClick={navigateProfile}>
+            <BlueBlock />
+            <span className="profile-text">Profile</span>
+          </div>
           <StartOrJoinGameModal
             isOpen={isStartOrJoinGameModalOpen}
             onClose={closeStartOrJoinGameModal}
